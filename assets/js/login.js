10e005ade1375d2b1adebe5b623d3a616bc00940
Jinzishun <jinzishun.1999@qq.com>
2022-02-08 19:57:43 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,75 +1,79 @@
-$(function () {
-    $('.login-box a').on('click', function () {
+$(function(){
+    //点击“去注册账号的链接”
+    $('#link_reg').on('click',function(){
         $('.login-box').hide()
         $('.reg-box').show()
     })
-    $('.reg-box a').on('click', function () {
-        $('.reg-box').hide()
+    //点击“去登录的链接”
+    $('#link_login').on('click',function(){
         $('.login-box').show()
+        $('.reg-box').hide()
     })
 
-    // console.log(layui);
-    let form = layui.form
-    // console.log(layui.form);
-    // verify()值是一个对象
+    //从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+    //通过form.verify()函数自定义校验规则
     form.verify({
-        // 属性是规则名称,值可以是数组或者函数
-        pwd: [
-            // 第一个值是校验规则
-            /^[\S]{6,15}$/,
-            // 报错信息
-            '密码长度为6-15个字符,且不能包含空格!'
-        ],
-        repwd: function (value) {
-            let pwd = $('.reg-box [name=password]').val()
-            if (value !== pwd) {
-                return '两次输入的密码不一致'
+        //自定义了一个叫做pwd的校验规则
+        pwd:[
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ] ,
+        //校验两次密码是否一致的规则
+        repwd:function(value){
+            //通过形参拿到的是确认密码框中的内容
+            //还需要拿到密码框中的内容
+            //如果判断失败，则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if(pwd!== value){
+                return '两次密码不一致！'
             }
         }
     })
 
-
-    // 注册axios
-    let layer = layui.layer
-    $('#form-reg').on('submit', function (e) {
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit',function(e){
+        //1.阻止默认的提交行为
         e.preventDefault()
-        axios({
-            method: 'POST',
-            url: '/api/reguser',
-            data: $('#form-reg').serialize(),
-        }).then((res) => {
-            // console.log(res.data);
-            // 判断状态码
-            if (res.data.status !== 0) {
-                return alert(res.data.message)
+        //2.发起Ajax的POST请求
+        $.post('/api/reguser',
+        {username:$('#form_reg [name=username]').val(),
+        password:$('#form_reg [name=password]').val()},
+        function(res){
+            if(res.status !==0){
+                return layer.msg(res.message)
             }
-            // 注册成功
-            // alert(res.data.message)
-            layer.msg('恭喜您,注册成功!')
-            // 清空表单
-            $('#form-reg')[0].reset()
-            // 切换到登录区域
-            $('.reg-box a').click()
+            layer.msg('注册成功')
         })
-
     })
 
-
-    // 登录
-    $('#form-login').on('submit', function (e) {
+    //监听登录表单的提交事件
+    $('#form_login').on('submit',function(e){
+        //1.阻止默认的提交行为
         e.preventDefault()
-        axios({
-            method: 'POST',
-            url: '/api/login',
-            data: $('#form-login').serialize()
-        }).then((res) => {
-            if (res.data.status != 0) {
-                return layer.msg(res.data.message)
+        //2.发起Ajax的POST请求
+        $.ajax({
+        url:'/api/login',
+        method:'POST',
+        //快速获取表单中的数据
+        data:$(this).serialize(),
+        success:function(res){
+            if(res.status !==0){
+                return layer.msg('登录失败')
             }
-            // 成功提示
-            layer.msg('恭喜您,登陆成功!')
+            layer.msg('登录成功')
+            localStorage.setItem('token',res.token)
             location.href = '/index.html'
-            localStorage.setItem('token', res.data.token)
+            
+            
+        }
         })
+        
     })
 })
+
+
+
+
+
